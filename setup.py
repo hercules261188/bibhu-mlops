471697a5c70176225230f327769c5d2d4ec3bc59
Munu_Guddu <hercules261188@gmail.com>
2023-11-26 20:51:17 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In general learning, aiming to learn and teach MLOps',
+    author='Bibhu-Das',
+    license='',
+)
